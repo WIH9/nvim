b7eebf3176762be719af1c72df74475a126ccd8d
William Hoang <williamhoang@live.co.uk>
2022-11-08 15:41:34 +0000

    initial setup with packer

---
+++ init.lua
@@ -0,0 +1,10 @@
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
