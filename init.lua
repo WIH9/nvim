76ae31990b30e5e30311f6f9ea898a6775dacf03
William Hoang <williamhoang@live.co.uk>
2022-11-08 15:44:27 +0000

    setup numbers

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
+--line numbers
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- tabs/indentation
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- search
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
-
-vim.opt.smartindent = true
 
 vim.opt.wrap = false
